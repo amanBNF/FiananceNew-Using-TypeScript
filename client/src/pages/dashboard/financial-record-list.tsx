a2f8a9a9e433c5a3f319112714f55a2be235a298
Aman Shukla <aman29shukla@gmail.com>
2025-07-01 20:21:53 +0530

    css edits

--- client/src/pages/dashboard/financial-record-list.tsx
+++ client/src/pages/dashboard/financial-record-list.tsx
@@ -1,3 +1,17 @@
+// import { useFinancialRecords } from "../../contexts/financial-record-contex";
+// import { useTable, Column, CellProps, Row } from 'react-table';
+
+
 export const FinancialRecordList = () => {
-    return <div>Record list</div>
+
+    // const {records} = useFinancialRecords();
+
+    // const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data: records})
+
+    return <div className="table-container">
+        <table className="table">
+            <thead></thead>
+            <tbody></tbody>
+        </table>
+    </div>
 };
